836fbe8e95611761cad8a948f7ac5ea562ab600b
usbin <rooprete99@naver.com>
2020-12-10 12:39:02 +0900

    change db port

--- js/databaseInfo.js
+++ js/databaseInfo.js
@@ -2,6 +2,7 @@ module.exports = (function () {
     return {
         local: {
             host        :   'localhost',
+            port        :   '3307',
             user        :   'root',
             password    :   '12551255',
             database    :   'monitoring'
